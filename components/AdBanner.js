8fa9eb94921fd51a930c1bb75887ea57b7fa677a
sandeep194920 <sandeepamaranath@gmail.com>
2021-10-10 22:06:09 -0400

    testing hard level implementation by not giving bulls in 1st attempt

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -29,7 +29,9 @@ const AdBanner = (props) => {
             <AdMobBanner
                 style={{ ...styles.ad, ...props.adStyle }}
                 bannerSize={props.bannerStyle}
-                adUnitID={Platform.OS === "ios" ? "ca-app-pub-7296629630933757/2926375308" : "ca-app-pub-7296629630933757/5957036786"}
+                // adUnitID={Platform.OS === "ios" ? "ca-app-pub-7296629630933757/2926375308" : "ca-app-pub-7296629630933757/5957036786"}
+                adUnitID={Platform.OS === "ios" ? "ca-app-pub-3940256099942544/6300978111" : "ca-app-pub-3940256099942544/6300978111"}
+
                 servePersonalizedAds
                 onDidFailToReceiveAdWithError={() => console.log("Banner error")} />
         </View>
--- context.js
+++ context.js
@@ -10,6 +10,7 @@ import {
     setTestDeviceIDAsync,
 } from 'expo-ads-admob';
 import { BackHandler } from 'react-native';
+import { computerWord, computerNumber } from './GameLogic/computerChoice';
 
 const AppContext = React.createContext()
 export const AppProvider = ({ children }) => {
@@ -60,10 +61,25 @@ export const AppProvider = ({ children }) => {
 
         // FOR HARD LEVEL
         // if the user word contains even a single bull, then it becomes easy to guess. So we need to re produce the computer word if the first attempt gets atleast one bull
+        while (attempts === 0 && game.difficulty === GAME.level.HARD && bull > 0) {
+            console.log(`This is first attemot`)
+            console.log(`New Computer choice is ${computerChoice}`);
 
-        // while (attempts === 1) {
-        //     console.log(`This is attempt 1`)
-        // }
+            let newCompChoice = ''
+            if (game.gameType === GAME.type.NUMBER) {
+                newCompChoice = computerNumber(game.letters)
+                console.log(`The comp number is ${newCompChoice}`)
+            } else if (game.gameType === GAME.type.WORD) {
+                newCompChoice = computerWord(game.letters)
+                console.log(`The comp word is ${newCompChoice}`)
+            }
+            setComputerChoice(newCompChoice)
+
+            bull = cowBullCount(newCompChoice, userWord).bull
+            cow = cowBullCount(newCompChoice, userWord).cow
+        }
+
+
 
         let word = {
             userWord,
