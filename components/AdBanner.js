5a25cbb3d8bc42834f90f28b177e092040c61572
sandeep194920 <sandeepamaranath@gmail.com>
2021-10-10 22:40:10 -0400

    ads resuming

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -29,9 +29,7 @@ const AdBanner = (props) => {
             <AdMobBanner
                 style={{ ...styles.ad, ...props.adStyle }}
                 bannerSize={props.bannerStyle}
-                // adUnitID={Platform.OS === "ios" ? "ca-app-pub-7296629630933757/2926375308" : "ca-app-pub-7296629630933757/5957036786"}
-                adUnitID={Platform.OS === "ios" ? "ca-app-pub-3940256099942544/6300978111" : "ca-app-pub-3940256099942544/6300978111"}
-
+                adUnitID={Platform.OS === "ios" ? "ca-app-pub-7296629630933757/2926375308" : "ca-app-pub-7296629630933757/5957036786"}
                 servePersonalizedAds
                 onDidFailToReceiveAdWithError={() => console.log("Banner error")} />
         </View>
